ad28be91b99bd4b9842d9f950a5146711c093021
jbbn <joaobbn@gmail.com>
2019-12-09 05:12:46 -0300

    feat: set up PostgreSQL datatase support

--- knexfile.js
+++ knexfile.js
@@ -33,4 +33,19 @@ module.exports = {
     seeds,
     useNullAsDefault: true,
   },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations,
+  },
 }
